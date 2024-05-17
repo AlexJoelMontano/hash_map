e743d006e7f4154bef265f089eb466958c99024c
AlexJoelMontano <alexjoelmontano@gmail.com>
2024-05-16 23:50:46 -0400

    Farm Manager CLI game used hash_map program and added a Farm class class has methods to run the game using a HashMap the program will generate a random amount of crops up to the player what they decide to do as manager game is over when 'done' is typed in Menu.

--- hash_map.rb
+++ hash_map.rb
@@ -6,7 +6,7 @@ class HashMap
 
   def hash(key)
     hash_code = 0
-    prime_number = 31
+    prime_number = 2029
 
     key.each_char { |char| hash_code = (hash_code * prime_number + char.ord) }
 
@@ -16,6 +16,19 @@ class HashMap
   def set(key, value)
     index = hash(key) % @size
     @buckets[index] = value
+
+    if length >= @size * 0.75
+      resize = @size * 2
+      new_buckets = Array.new(resize)
+
+      @buckets.compact.each do |old_key, old_value|
+        new_index = hash(old_key) % resize
+        new_buckets[new_index] = old_value
+      end
+
+      @size = resize
+      @buckets = new_buckets
+    end
   end
 
   def get(key)
@@ -36,12 +49,9 @@ class HashMap
     count = 0
 
     @buckets.each do |bucket|
-      if bucket.nil?
-        count
-      else
-        count += 1
-      end
+      count += 1 unless bucket.nil?
     end
+
     count
   end
 
@@ -63,11 +73,131 @@ class HashMap
     values
   end
 
-  def indexes
-    indexes = {}
-    @buckets.each_with_index do |bucket,i|
-      indexes[i] = bucket unless bucket.nil?
+  def keys
+    keys = []
+
+    @buckets.each do |key, _|
+      keys << key unless key.nil?
     end
-    indexes
+
+    keys
+  end
+end
+
+Inventory = HashMap.new
+
+#scenario of random amounts of crops
+Crops = ["wheat", "corn", "rice", "soybeans", "beans", "barley", "oats", "peanuts", "beans", "strawberries", "blueberries", "raspberries"]
+Crops.each do |crop|
+  random = rand(1..1000)
+  Inventory.set(crop,random)
+end
+
+class Farm
+
+  def initialize
+    @crops = Crops
+    @money = 100
+    @price = rand(1..10)
+  end
+
+  def amount(crop)
+    return Inventory.get(crop)
+  end
+
+  def sell(crop)
+    puts "You currently have #{amount(crop)}"
+    puts "How many #{crop} do you want to sell?"
+    selling = gets.chomp.to_i
+
+    if selling > amount(crop)
+      puts "You cannot sell more than you have enter a lower amount!"
+    else
+      inv = amount(crop)
+      remain = inv - selling
+      Inventory.set(crop, remain)
+
+      puts "#{crop} is selling for #{@price} per unit"
+      profit = selling * @price
+      @money = @money + profit
+      puts "You made #{profit} for selling #{selling} #{crop} you now have #{@money} in your wallet"
+    end
+
+    puts "You now have #{amount(crop)} remaining"
+  end
+
+  def view
+    p crops
+  end
+
+  def money
+    return @money
+  end
+
+  def buy(crop)
+    puts "How many units do you want to buy?"
+    buying = gets.chomp.to_i
+    buying_price = @price + rand(1..5)
+    bought = buying * buying_price
+    puts "#{crop} cost #{buying_price} per unit for a total of #{bought}, do you want to buy? (yes/no)"
+    buy_choice = gets.chomp
+
+
+    if buy_choice == 'yes'
+      @money = @money - bought
+
+      current = Inventory.get(crop)
+      new_amount = current + buying
+      Inventory.set(crop,new_amount)
+
+      puts "You now have #{amount(crop)} in your inventory and #{@money} in your wallet"
+    else
+      puts "Nothing was bought you still have #{amount(crop)} in your inventory"
+    end
+  end
+end
+
+farm = Farm.new
+puts "_____________________________________"
+puts "Farming Manager Simulator CLI edition"
+puts "------------------------------------------------------------------------------------"
+puts "This game will prompt you if you want to Buy, Sell, or View your inventory of crops."
+puts "------------------------------------------------------------------------------------"
+puts "Crops:"
+p Crops
+game_choice = " "
+
+#farm game that will keep prompting until 'done' is entered
+
+until game_choice == 'done'
+  puts ' '
+  puts "Menu: |view|  |sell|  |buy|  |wallet|"
+  game_choice = gets.chomp
+
+  if game_choice == 'view'
+    puts " "
+    puts "Which crop do you want to view?"
+
+    crop = gets.chomp
+    puts "You have #{farm.amount(crop)} #{crop}"
+    game_choice = crop
+  elsif game_choice == 'sell'
+    puts "Which crop do you want to sell?"
+
+    crop = gets.chomp
+
+    farm.sell(crop)
+    game_choice = crop
+
+  elsif game_choice == 'buy'
+    puts "Which crop do you want to buy?"
+
+    crop = gets.chomp
+
+    farm.buy(crop)
+    game_choice = crop
+  else
+    puts "You have $#{farm.money} in your wallet"
+
   end
 end
